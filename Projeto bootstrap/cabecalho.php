c1b1276b34e502b6a7754c1ed57e4ea5afeaa381
Igor Luigi Fracarolli <91166454+igorluigi2003@users.noreply.github.com>
2022-09-30 12:10:16 -0300

    Add files via upload

---
+++ Projeto bootstrap/cabecalho.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    <link href="./estilo.css" rel="stylesheet" />
+    <link href="./bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+  </body>
+    <div id="container"> 
+        <header id="cabecalho">
+            <div id="logo"> 
+                <img src="./contato.png" width="50" height="50"/>
+            </div>
+            <nav> 
+                <ul class="menu">
+                <li>
+                        <a href="./index.php"> Início </a>
+                    </li>
+                    <li>
+                        <a href="./contato.php"> Contato </a>
+                    </li>
+                    <li>
+                        <a href="./usuarios.php"> Usuarios </a>
+                    </li>
+                    <li>
+                        <a href="./produtos.php"> Produtos </a>
+                    </li>
+                    <li>
+                        <a href="./clientes.php"> Clientes </a>
+                    </li>
+                    <li>
+                        <a href="./vendas.php"> Vendas </a>
+                    </li>
+                    <li>
+                        <a href="./estoque.php"> Estoque </a>
+                    </li>
+                    <li>
+                        <a href="./redes.php"> Link Menu </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    <main id="conteudo">
---
+++ Projeto bootstrap/rodape.php
@@ -0,0 +1,4 @@
+</div> <!-- Está div fecha a div class cointainer -->
+<script src="./js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
